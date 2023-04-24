11623cf8f9e22c694da2d8a1f5f0849fc6a4b2cf
SongGayoung <xjongam1x@gmail.com>
2023-04-24 21:53:10 +0900

    added: list styling

--- src/components/PoketmonList.tsx
+++ src/components/PoketmonList.tsx
@@ -1,17 +1,48 @@
 import React from "react";
 import styled from "@emotion/styled/macro";
 
-const Base = styled.div``;
+const Base = styled.div`
+  margin-top: 24px;
+`;
 
-const ListItem = styled.li``;
+const ListItem = styled.li`
+  position: relative;
+  list-style: none;
+  display: flex;
+  align-items: center;
+  box-shadow: 6px 4px 14px 5px rgba(0, 0, 0, 0.12);
+  border-radius: 12px;
+  & + & {
+    margin-top: 18px;
+  }
+`;
 
-const List = styled.ul``;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
 
 const Image = styled.img``;
 
-const Name = styled.p``;
+const Name = styled.p`
+  margin: 0;
+  padding: 0 0 0 12px;
+  flex: 1 1 100%;
+  color: #374151;
+  text-transform: capitalize;
+  font-size: 16px;
+  font-weight: bold;
+`;
 
-const Index = styled.p``;
+const Index = styled.p`
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  right: 16px;
+  font-size: 24px;
+  font-weight: bold;
+  color: #d1d5db;
+`;
 
 //포켓몬 이미지를 가져오는 함수
 const getImageUrl = (index: number): string =>
